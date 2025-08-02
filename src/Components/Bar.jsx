14265787c5983ffae7233911685e3fa1438b5b36
Yoonis <yoonkagallad2021@gmail.com>
2025-08-02 22:23:34 +0300

    Dashboard

--- src/Components/Bar.jsx
+++ src/Components/Bar.jsx
@@ -1,6 +1,6 @@
-function Bar(){
+function Bar(props){
     return <div>
-        <i className="fa-solid fa-bars text-3xl ml-44 mt-2"></i>
+        <i className={props.bar}></i>
     </div>
 }
 
--- src/Components/SlideBar.jsx
+++ src/Components/SlideBar.jsx
@@ -3,7 +3,7 @@ import Dashboard from "./Dashboard"
 
 function SlideBar(props){
     return <div className="bg-[#557a46] w-56 h-[90vh] text-white shadow-sm">
-        <Bar/>
+        <Bar bar="fa-solid fa-bars text-3xl ml-44 mt-2"/>
         <Dashboard icon1="fa-solid fa-box text-2xl" name="Dashboard"/>
         <Dashboard icon1="fa-brands fa-product-hunt text-2xl" name="Product"/>
         <Dashboard icon1="fa-solid fa-hospital text-2xl" name="Service"/>
